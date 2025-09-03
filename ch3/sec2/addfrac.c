f5594031a1840f0167bd52c8b607eb0713b4b6a8
Troy Caro <troy.caro@pitt.edu>
2025-09-03 12:20:51 -0400

    addfrac.c

---
+++ ch3/sec2/addfrac.c
@@ -0,0 +1,20 @@
+/* Add two fractions */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, results_denom;
+
+    printf("Enter the first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+
+    printf("Enter the second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+
+    result_num = num1 * denom2 + num2 * denom1;
+    results_denom = denom1 * denom2;
+
+    printf("The sum is %d/%d\n", result_num, results_denom);
+    return 0;
+}
